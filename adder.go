e371b9cfc99289003d5f996f2720264969036be5
Justin Kim <yulli@yulli.org>
2015-12-12 01:20:49 -0500

    Add adder

---
+++ adder.go
@@ -0,0 +1,5 @@
+package adder
+
+func Add(n1, n2 int) int {
+	return n1 + n2
+}
---
+++ adder_test.go
@@ -0,0 +1,52 @@
+package adder
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		n1   int
+		n2   int
+		want int
+	}{
+		{
+			n1:   0,
+			n2:   0,
+			want: 0,
+		},
+		{
+			n1:   1,
+			n2:   0,
+			want: 1,
+		},
+		{
+			n1:   -1,
+			n2:   0,
+			want: -1,
+		},
+		{
+			n1:   3,
+			n2:   -3,
+			want: 0,
+		},
+		{
+			n1:   5,
+			n2:   16,
+			want: 21,
+		},
+		{
+			n1:   694,
+			n2:   1,
+			want: 695,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Logf("[%02d] test %d + %d", i, tt.n1, tt.n2)
+		sum := Add(tt.n1, tt.n2)
+		if sum != tt.want {
+			t.Errorf("unexpected value:\n- want: %v\n- got: %v", tt.want, sum)
+		}
+	}
+}
